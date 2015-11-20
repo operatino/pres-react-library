97ae1ea1f22633868881b6251cba9ac17299cf46
Robert Haritonov <r@rhr.me>
2015-11-20 14:00:06 +0100

    update logo, add material-ui gh screenshot

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -16,6 +16,7 @@ const images = {
   libraries: require("./libraries.jpg"),
   libraryBg: require("./library-bg.jpg"),
   materialUI: require("./material-ui.png"),
+  materialUIGH: require("./material-ui-gh.png"),
   reactSelect: require("./react-select.png"),
   componetizing: require("./componetizing.jpg"),
   react: require("./react.png"),
@@ -23,6 +24,7 @@ const images = {
   playground: require("./playground.png"),
   github: require("./github.svg"),
   puzzle: require("./puzzle.jpg"),
+  reactAmsterdam: require("./react-amsterdam.png"),
   tools: require("./tools.jpg")
 };
 
@@ -31,8 +33,8 @@ preloader([images.lg]);
 export default class extends React.Component {
   render() {
     return (
-      <Deck transition={["slide", "spin"]} transitionDuration={800} progress={"number"}>
-        <Slide transition={["zoom"]} bgColor="primary">
+      <Deck transition={["slide", "spin"]} transitionDuration={800} progress={"bar"}>
+        <Slide transition={["zoom"]} bgColor="primary" width={1200px}>
           <Heading size={1} fit caps lineHeight={1} textColor="black">
             Building React
           </Heading>
@@ -51,13 +53,13 @@ export default class extends React.Component {
 
 
         <Slide bgImage={images.sourcejs.replace("/", "")}>
-          <Image src={images.sourcejsLogo} margin="0 0 30px" width="800px" />
+          <Image src={images.sourcejsLogo} margin="0 0 30px" width="800px"/>
           <Text textSize="4em" textColor="white" bold>Author of the Style Guide Platform</Text>
         </Slide>
 
 
         <Slide transition={["slide"]} bgColor="#2D2D2D" align="flex-start center">
-            <Image src={images.libraries.replace("/", "")} width="100%" />
+          <Image src={images.libraries.replace("/", "")} width="100%"/>
         </Slide>
 
 
@@ -83,7 +85,7 @@ export default class extends React.Component {
 
 
         <Slide bgColor="white" align="flex-start flex-start">
-            <Image src={images.materialUI.replace("/", "")} width="100%" />
+          <Image src={images.materialUI.replace("/", "")} width="100%"/>
         </Slide>
 
 
@@ -94,15 +96,25 @@ export default class extends React.Component {
 
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary" align="flex-start center">
           <List margin="-100px 0 0" textSize="1.5em">
-            <ListItem><Link href="http://callemall.github.io/material-ui" textColor="primary">Material UI</Link></ListItem>
-            <ListItem><Link href="http://nikgraf.github.io/belle" textColor="primary">Belle</Link></ListItem>
-            <ListItem><Link href="http://elemental-ui.com/" textColor="primary">Elemental UI</Link></ListItem>
-            <ListItem><Link href="http://react-components.com/" textColor="primary">React Components (collection)</Link></ListItem>
+            <ListItem><Link href="http://callemall.github.io/material-ui" textColor="primary">Material
+              UI</Link></ListItem>
+            <ListItem><Link href="http://nikgraf.github.io/belle"
+                            textColor="primary">Belle</Link></ListItem>
+            <ListItem><Link href="http://elemental-ui.com/" textColor="primary">Elemental
+              UI</Link></ListItem>
+            <ListItem><Link href="http://react-components.com/" textColor="primary">React Components
+              (collection)</Link></ListItem>
           </List>
         </Slide>
 
 
-        <Slide bgColor="black" transition={["slide"]} bgImage={images.componetizing.replace("/", "")} bgDarken={0.75}>
+        <Slide bgColor="white" align="flex-start flex-start">
+          <Image src={images.materialUIGH.replace("/", "")} width="100%"/>
+        </Slide>
+
+
+        <Slide bgColor="black" transition={["slide"]} bgImage={images.componetizing.replace("/", "")}
+               bgDarken={0.75}>
           <Heading size={1} caps fit textColor="primary" margin="-60px 0 0">
             Componetizing
           </Heading>
@@ -113,11 +125,12 @@ export default class extends React.Component {
 
 
         <Slide bgColor="#2D2D2D" transition={["slide"]} align="flex-start center">
-            <Image src={images.react.replace("/", "")} width="500px" />
+          <Image src={images.react.replace("/", "")} width="500px"/>
         </Slide>
 
 
-        <Slide bgColor="black" transition={["slide"]} bgImage={images.composable.replace("/", "")} bgDarken={0.75}>
+        <Slide bgColor="black" transition={["slide"]} bgImage={images.composable.replace("/", "")}
+               bgDarken={0.75}>
 
           <Heading size={3} caps fit textColor="tertiary">
             Patterns
@@ -159,11 +172,18 @@ export default class extends React.Component {
             margin="20px auto"/>
         </Slide>
 
+        <Slide transition={["fade"]} bgColor="black">
+          <Heading>
+            and...
+          </Heading>
+        </Slide>
+
 
         <Slide transition={["fade"]} bgColor="black">
           <BlockQuote>
-            <Quote textSize="3em" style={{lineHeight: "1.3"}}>I want you to take a moment and forget everything you know about web development.<br/>
-            Keep an open mind.</Quote>
+            <Quote textSize="3em" style={{lineHeight: "1.3"}}>I want you to take a moment and forget
+              everything you know about web development.<br/>
+              Keep an open mind.</Quote>
             <Cite>Christopher Chedeau @vjeux</Cite>
           </BlockQuote>
         </Slide>
@@ -171,7 +191,7 @@ export default class extends React.Component {
 
         <Slide bgColor="secondary" textColor="primary" align="flex-start center">
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
-              CSS in JS
+            CSS in JS
           </Heading>
           <Appear fid="1">
             <Heading size={2} caps fit textColor="tertiary">
@@ -194,16 +214,17 @@ export default class extends React.Component {
 
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary" align="flex-start center">
           <List margin="-100px 0 0" textSize="1.5em">
-            <ListItem><Link href="http://projects.formidablelabs.com/radium/" textColor="primary">Radium</Link></ListItem>
-            <ListItem><Link href="https://github.com/js-next/react-style" textColor="primary">React Style</Link></ListItem>
+            <ListItem><Link href="http://projects.formidablelabs.com/radium/"
+                            textColor="primary">Radium</Link></ListItem>
+            <ListItem><Link href="https://github.com/js-next/react-style" textColor="primary">React
+              Style</Link></ListItem>
             <ListItem><Link href="https://github.com/jsstyles/jss" textColor="primary">JSS</Link></ListItem>
           </List>
         </Slide>
 
 
-
         <Slide bgColor="black" transition={["slide"]} bgImage={images.tools.replace("/", "")} bgDarken={0.75}>
-          <Heading size={3} caps fit textColor="primary" >
+          <Heading size={3} caps fit textColor="primary">
             Tools
           </Heading>
         </Slide>
@@ -211,7 +232,7 @@ export default class extends React.Component {
 
         <Slide bgColor="secondary" textColor="primary" align="flex-start center">
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
-              Workbench
+            Workbench
           </Heading>
           <Appear fid="1">
             <Heading size={2} caps fit textColor="tertiary">
@@ -234,13 +255,15 @@ export default class extends React.Component {
 
 
         <Slide bgColor="white" align="flex-start flex-start">
-            <Link href="http://projects.formidablelabs.com/component-playground/"><Image src={images.playground.replace("/", "")} width="80%" /></Link>
+          <Link href="http://projects.formidablelabs.com/component-playground/"><Image
+            src={images.playground.replace("/", "")} width="80%"/></Link>
         </Slide>
 
 
-        <Slide bgColor="secondary" textColor="primary" align="flex-start center" bgImage={images.puzzle.replace("/", "")} bgDarken={0.75}>
+        <Slide bgColor="secondary" textColor="primary" align="flex-start center"
+               bgImage={images.puzzle.replace("/", "")} bgDarken={0.75}>
           <Heading size={1} fit caps lineHeight={1} textColor="tertiary">
-              Putting all together
+            Putting all together
           </Heading>
           <Appear fid="1">
             <Heading size={2} caps fit textColor="primary">
@@ -269,8 +292,14 @@ export default class extends React.Component {
         </Slide>
 
 
+        <Slide transition={["slide"]} bgColor="white" align="flex-start flex-start">
+          <Image src={images.reactAmsterdam.replace("/", "")} margin="70px auto 30px" height="300px"/>
+          <Text textSize="4em" textColor="black" bold>Amsterdam React Conference</Text>
+        </Slide>
+
+
         <Slide bgImage={images.sourcejs.replace("/", "")}>
-          <Image src={images.github} width="250px" />
+          <Image src={images.github} width="250px"/>
           <Link href="http://bit.ly/sourcejs-loves-react"><Text textSize="4em" textColor="white" bold>bit.ly/sourcejs-loves-react</Text></Link>
           <Text textColor="white" textSize="3em" margin="1em 0px 0px">Robert Haritonov</Text>
           <Text textColor="white" textSize="3em" margin="0 0px 0px">@operatino, r@rhr.me</Text>
